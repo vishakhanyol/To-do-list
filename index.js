f7e42787a47418dd5b3e3e4777743921e0dbca31
vishakhanyol <vishakhanyol@gmail.com>
2024-02-04 18:39:01 +0530

    new

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let inp=document.querySelector('#inp');
-let list=document.querySelector('#list');
-inp.addEventListener("keyup",function(event){
-    if(event.key=="return"){
-        addItem(this.value)
-        this.value=""
-    }
+let inp = document.querySelector('#inp');
+let list = document.querySelector('#list');
 
+inp.addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        addItem(this.value);
+        this.value = "";
+    }
 })
 
-let addItem = (inp)=>{
-    let listItem=document.createElement("li");
-    listItem.innerHTML='${inp}<i><i>';
-        list.appendChild(listItem);
+let addItem = (inp) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${inp}<i></i>`;
+    list.appendChild(listItem);
 }
